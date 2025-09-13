7f4effe7ee875c2ec8192bfbf65421c7aa394e8b
nikhileshchenna <chennanikhilesh@gmail.com>
2025-09-13 23:03:34 +0530

    message

--- gcd.java
+++ gcd.java
@@ -1,29 +1,21 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class gcd {
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
-        
          String s = sc.nextLine();
-
          String arr[]=s.split(" ");
         int sum=0;
          for(int i=0;i<arr.length;i++){
             int count=0;
             for(int j=0;j<arr[i].length();j++){
-
                 if(Character.isLetter(arr[i].charAt(j))){
                     count++;
                 }
-
-                
             }
             if(count>=sum){
                 sum=count;
             }
-
          }
-         System.out.println(sum);
-      
+         System.out.println(sum+" ");
     }
 }
